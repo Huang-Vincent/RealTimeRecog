d2755f2b6703cb11b28f888f7126fcb9472038e3
Vincent Huang <vinhuang848@gmail.com>
2019-08-14 14:45:13 -0400

    A

--- image.py
+++ image.py
@@ -1,2 +1,9 @@
 import cv2
 from keras.models import load_model
+import matplotlib.pyplot as plt
+
+def prepare(filepath):
+    IMG_SIZE = 28
+    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE), interpolation=cv2.INTER_AREA)
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
