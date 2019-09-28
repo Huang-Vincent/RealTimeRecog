57e2089ea5ff02c1bde5675404a5f75a011d227d
Vincent Huang <vinhuang848@gmail.com>
2019-09-28 12:25:12 -0400

    Updated image processing

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ class DrawInput(Widget):
         print(touch)
         if touch.button == 'left':
             with self.canvas:
-                Color(0, 0, 0)
-                touch.ud["line"] = Line(point=(touch.x, touch.y), width = 10)
+                Color(1, 1, 1)
+                touch.ud["line"] = Line(point=(touch.x, touch.y), width = 5)
         else:
             self.canvas.clear();
 
@@ -29,10 +29,9 @@ class Application(App):
     def build(self):
         return DrawInput()
 
-Window.clearcolor = (128, 128, 128, 128)
 Config.set('input', 'mouse', 'mouse, multitouch_on_demand')
 Config.set('graphics', 'resizable', False)
-Config.set('graphics', 'width', '500')
-Config.set('graphics', 'height', '500')
+Config.set('graphics', 'width', '280')
+Config.set('graphics', 'height', '280')
 if __name__ == "__main__":
     Application().run()
